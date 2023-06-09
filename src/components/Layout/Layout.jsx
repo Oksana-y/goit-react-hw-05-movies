83a8ac5a5fde31efd64bfbd521cbbef6933eef0c
Oksana Yerep <114570807+Oksana-y@users.noreply.github.com>
2023-06-09 19:41:04 +0300

    Update Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,21 +1,12 @@
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import React, { Suspense } from 'react';
-import { Header, Button, Main } from './Layout.styled';
+import { Header, Main } from './Layout.styled';
 import Navigation from './Navigation';
 
 export const Layout = () => {
   return (
     <>
       <Header>
-        {/* {location.pathname !== '/' && (
-          <Button
-            type="button"
-            onClick={goBackButtonHandler}
-          >
-            Go back
-          </Button>
-        )} */}
-
         <Navigation />
       </Header>
       <Main>
